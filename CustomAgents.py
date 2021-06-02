73fe4388b83b9a9f63f632e314ac2559193a86c8
walkerl <walkerl@union.edu>
2021-06-02 16:13:55 -0400

    created the Generous agent
    the generous agent is one that always donates a high amount to its opponent

---
+++ CustomAgents.py
@@ -0,0 +1,52 @@
+import random
+
+
+class GenerousAgent:
+    """
+    A Generous agent is one that loves to donate everything
+    they got (or almost everything they got). There is a
+    little bit of variance in its determined donation so that
+    Generous agents have a less likely chance to tie (and
+    therefore do not kill themselves consistently).
+    """
+
+    def __init__(self):
+        """
+        base constructor of a Generous agent
+        an agent with no history and no savings (and no sanity)
+        """
+        self.savings = 0
+        self.hist1 = 0
+        self.hist2 = 0
+        self.hist3 = 0
+
+    def reset(self):
+        """
+        reverts the agent back to its initiation state
+        """
+        self.savings = 0
+        self.hist1 = 0
+        self.hist2 = 0
+        self.hist3 = 0
+
+    def donate(self, other, round):
+        """
+        calculates the donation of this agent for one round,
+        the donation being a HIGH amount
+        :param other: the agent who will receive the donation
+        :param round: the current round number
+        :return: a donation from this agent to another
+        """
+        donation = random.randint(490, 500)
+
+        self.hist3 = self.hist2
+        self.hist2 = self.hist1
+        self.hist1 = donation
+        return donation
+
+    def add_savings(self, amount):
+        """
+        adding a specified amount to the agent's savings
+        :param amount: the value to add to agent's total savings
+        """
+        self.savings += amount
