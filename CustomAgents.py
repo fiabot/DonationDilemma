1c0a8140ac29fe2505d282053f61a7cf63df7912
walkerl <walkerl@union.edu>
2021-06-02 16:34:24 -0400

    added Stingy agent
    the Stingy agent is the opposite of the Generous agent, it likes to hold onto all of its money

--- CustomAgents.py
+++ CustomAgents.py
@@ -1,4 +1,5 @@
 import random
+from Agent import Agent
 
 
 class GenerousAgent:
@@ -38,6 +39,57 @@ class GenerousAgent:
         :return: a donation from this agent to another
         """
         donation = random.randint(490, 500)
+
+        self.hist3 = self.hist2
+        self.hist2 = self.hist1
+        self.hist1 = donation
+        return donation
+
+    def add_savings(self, amount):
+        """
+        adding a specified amount to the agent's savings
+        :param amount: the value to add to agent's total savings
+        """
+        self.savings += amount
+
+
+class StingyAgent:
+    """
+    A Stingy agent is one that hates donating their money.
+    A Stingy agent wants to keep onto all (or nearly all) of its money.
+    There is a little bit of variance in its determined donation so that
+    Stingy agents have a less likely chance to tie (and
+    therefore do not kill themselves consistently).
+    """
+
+    def __init__(self):
+        """
+        base constructor of a Stingy agent
+        an agent with no history and no savings (and no sanity)
+        """
+        self.savings = 0
+        self.hist1 = 0
+        self.hist2 = 0
+        self.hist3 = 0
+
+    def reset(self):
+        """
+        reverts the Stingy agent back to its initiation state
+        """
+        self.savings = 0
+        self.hist1 = 0
+        self.hist2 = 0
+        self.hist3 = 0
+
+    def donate(self, other, round):
+        """
+        calculates the donation of this agent for one round,
+        the donation being a LOW amount
+        :param other: the agent who will receive the donation
+        :param round: the current round number
+        :return: a donation from this agent to another
+        """
+        donation = random.randint(0, 10)
 
         self.hist3 = self.hist2
         self.hist2 = self.hist1
