fc828e1f2a11a430419b4953ee10d0d56fa1bc50
walkerl <walkerl@union.edu>
2021-06-02 17:37:52 -0400

    created the Averaging Agent
    an agent that returns the value that is the average between this agent's previous donation and the opponent's previous donation

--- CustomAgents.py
+++ CustomAgents.py
@@ -157,3 +157,59 @@ class TitForTatAgent:
         :param amount: the value to add to agent's total savings
         """
         self.savings += amount
+
+
+class AveragingAgent:
+    """
+    This agent is a custom agent by Logan, donating the amount that
+    is the average amount between this agent's donation of the previous
+    round and the opponent's donation of the previous round. If it
+    is the very first round (and therefore no donations have been
+    preformed yet), the agent acts randomly.
+    """
+
+    def __init__(self):
+        """
+        base constructor of a Averaging agent
+        an agent with no history and no savings (and no sanity)
+        """
+        self.savings = 0
+        self.hist1 = 0
+        self.hist2 = 0
+        self.hist3 = 0
+
+    def reset(self):
+        """
+        reverts the agent back to its initiation state
+        """
+        self.savings = 0
+        self.hist1 = 0
+        self.hist2 = 0
+        self.hist3 = 0
+
+    def donate(self, other, round):
+        """
+        calculates the donation of this agent for one round,
+        the donation being the average of this agent's and the
+        opponent's immeadiately previous amount or
+        a random donation should it be the first round.
+        :param other: the agent who will receive the donation
+        :param round: the current round number
+        :return: a donation from this agent to another
+        """
+        if round == 0:
+            donation = random.randint(0, 500)
+        else:
+            donation = (self.hist1 + other.hist1)/2
+
+        self.hist3 = self.hist2
+        self.hist2 = self.hist1
+        self.hist1 = donation
+        return donation
+
+    def add_savings(self, amount):
+        """
+        adding a specified amount to the agent's savings
+        :param amount: the value to add to agent's total savings
+        """
+        self.savings += amount
